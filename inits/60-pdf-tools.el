e0337f2afa647a3b47db62d4456d566ca3afa52c
Zhenjun Wang <zjwang0414@gmail.com>
2016-07-06 13:23:06 +0900

    add pdf view

---
+++ inits/60-pdf-tools.el
@@ -0,0 +1,26 @@
+(require 'pdf-tools)
+(require 'pdf-annot)
+(require 'pdf-history)
+(require 'pdf-info)
+(require 'pdf-isearch)
+(require 'pdf-links)
+(require 'pdf-misc)
+(require 'pdf-occur)
+(require 'pdf-outline)
+(require 'pdf-sync)
+(require 'tablist-filter)
+(require 'tablist)
+(add-to-list 'auto-mode-alist (cons "\\.pdf$" 'pdf-view-mode))
+
+(add-hook 'pdf-view-mode-hook
+  (lambda ()
+    (pdf-misc-size-indication-minor-mode)
+    (pdf-links-minor-mode)
+    (pdf-isearch-minor-mode)
+    (pdf-view-midnight-minor-mode)
+  )
+)
+
+(setq pdf-view-midnight-colors '("#e3e2d5" . "#505050"))
+
+(provide '60-pdf-tools)
