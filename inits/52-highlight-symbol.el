faa7731567eec9182ff89c2ca9d730bc070db5d7
Zhenjun Wang <zjwang0414@gmail.com>
2016-07-06 13:22:51 +0900

    add highlight variables

---
+++ inits/52-highlight-symbol.el
@@ -0,0 +1,6 @@
+(require 'highlight-symbol)
+
+(global-set-key (kbd "<f3>") 'highlight-symbol-at-point)
+(global-set-key (kbd "M-<f3>") 'highlight-symbol-remove-all)
+
+(provide '52-highlight-symbol)
