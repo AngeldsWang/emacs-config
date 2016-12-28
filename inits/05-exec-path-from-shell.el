45d2b1bb1c4cb7b5458522780ad4a790a5f32290
Zhenjun Wang <zjwang0414@gmail.com>
2016-12-28 10:28:57 +0900

    avoid show error message

--- inits/05-exec-path-from-shell.el
+++ inits/05-exec-path-from-shell.el
@@ -1,5 +1,6 @@
 (require 'exec-path-from-shell)
 (when (memq window-system '(mac ns))
+  (setq exec-path-from-shell-arguments '("-l"))
   (exec-path-from-shell-initialize)
   (exec-path-from-shell-copy-env "GOPATH"))
 
