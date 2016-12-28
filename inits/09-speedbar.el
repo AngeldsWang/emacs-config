d1d4b123bfa6b6f3715cbfd5f94c6a57a222d7e8
Zhenjun Wang <zjwang0414@gmail.com>
2016-12-28 10:32:33 +0900

    delete speedbar

--- inits/09-speedbar.el
+++
@@ -1,4 +0,0 @@
-(require 'sr-speedbar)
-(global-set-key (kbd "s-s") 'sr-speedbar-toggle)
-
-(provide '09-speedbar)
--- inits/10-projectile.el
+++ inits/10-projectile.el
@@ -6,7 +6,7 @@
 (require 'helm-projectile)
 (helm-projectile-on)
 
-(require 'projectile-speedbar)
+;;(require 'projectile-speedbar)
 (add-hook 'projectile-mode-hook 'projectile-rails-on)
 (require 'go-projectile)
 
