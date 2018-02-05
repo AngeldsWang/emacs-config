1dc6aed78ef30e85c5e2132b9db9ac04f3fbe217
Zhenjun Wang <zjwang0414@gmail.com>
2018-02-05 10:33:47 +0900

    enable helm-ag-use-agignore

--- inits/07-helms.el
+++ inits/07-helms.el
@@ -37,6 +37,7 @@
 (custom-set-variables
  '(helm-ag-base-command "ag --nocolor --nogroup --ignore-case")
  '(helm-ag-command-option "--all-text")
- '(helm-ag-insert-at-point 'symbol))
+ '(helm-ag-insert-at-point 'symbol)
+ '(helm-ag-use-agignore t))
 
 (provide '07-helms)
