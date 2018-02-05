932aa34d064f7d702ce9b7a086676865c474f0cd
Zhenjun Wang <zjwang0414@gmail.com>
2018-02-05 10:30:49 +0900

    take a shot of helm-tramp

---
+++ inits/53-tramp.el
@@ -0,0 +1,20 @@
+(setq tramp-default-method "ssh")
+
+(add-hook 'helm-tramp-pre-command-hook '(lambda ()
+                                          (projectile-mode 0)))
+(add-hook 'helm-tramp-quit-hook '(lambda ()
+                                   (projectile-mode 1)))
+
+
+;; ;; speed up tramp
+;; (setq remote-file-name-inhibit-cache nil)
+;; (setq vc-ignore-dir-regexp
+;;       (format "%s\\|%s"
+;;                     vc-ignore-dir-regexp
+;;                     tramp-file-name-regexp))
+;; (setq tramp-verbose 1)
+
+(setq make-backup-files nil)
+(setq create-lockfiles nil)
+
+(provide '53-tramp)
