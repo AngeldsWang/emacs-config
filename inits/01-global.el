c8895db0415421b37f3aeee0a348b8de52999829
Zhenjun Wang <zjwang0414@gmail.com>
2018-02-05 10:44:20 +0900

    add some util functions

--- inits/01-global.el
+++ inits/01-global.el
@@ -2,7 +2,14 @@
 (tool-bar-mode -1)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (add-hook 'window-setup-hook 'toggle-frame-fullscreen t)
-(load-theme 'zenburn t)
+
+;;(load-theme 'zenburn t)
+(require 'doom-themes)
+(load-theme 'doom-sora t)
+(doom-themes-visual-bell-config)
+(doom-themes-neotree-config)
+(doom-themes-org-config)
+
 (scroll-bar-mode -1)
 (eval-when-compile (require 'cl))
 ;; font and size
@@ -11,7 +18,7 @@
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 ;; Debug on init
-;;(setq debug-on-error t)
+;; (setq debug-on-error t)
 (global-hl-line-mode 1)
 (setq indent-line-function 'insert-tab)
 
@@ -39,6 +46,30 @@
 
 (advice-add 'kill-ring-save :before #'slick-copy)
 
+(defun toggle-line-spacing ()
+  "Toggle line spacing between no extra space to extra half line height.
+URL `http://ergoemacs.org/emacs/emacs_toggle_line_spacing.html'
+Version 2017-06-02"
+  (interactive)
+  (if line-spacing
+      (setq line-spacing nil)
+    (setq line-spacing 0.5))
+  (redraw-frame (selected-frame)))
+
+(defun rename-file-and-buffer ()
+  "Rename the current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
+(global-set-key (kbd "C-c r")  'rename-file-and-buffer)
+
 (setq ag-highlight-search t)
 
 (require 'yasnippet)
@@ -47,6 +78,11 @@
 
 ;;(add-hook 'after-init-hook 'global-company-mode)
 ;;(add-to-list 'company-backends 'company-tern)
+
+(require 'company)
+(global-company-mode)
+(global-set-key (kbd "TAB") #'company-indent-or-complete-common)
+(add-to-list 'company-backends 'company-tern)
 
 (require 'desktop)
 (setq desktop-path (list "~/.emacs-server"))
@@ -70,7 +106,64 @@
 (global-set-key (kbd "C-M-n") 'new-frame)
 (global-set-key (kbd "M-`") 'other-frame)
 
+;; insert into rectangle
+(global-set-key (kbd "C-x r i") 'string-insert-rectangle)
+
 
 (setq redisplay-dont-pause nil)
+
+(defvar my-ratio-dict
+  '((1 . 1.61803398875)
+    (2 . 2)
+    (3 . 3)
+    (4 . 4)
+    (5 . 0.61803398875))
+  "The ratio dictionary.")
+
+(defun my-split-window-horizontally (&optional ratio)
+  "Split window horizontally and resize the new window.
+'C-u number M-x my-split-window-horizontally' uses pre-defined
+ratio from `my-ratio-dict'.
+Always focus on bigger window."
+  (interactive "P")
+  (let* (ratio-val)
+    (cond
+     (ratio
+      (setq ratio-val (cdr (assoc ratio my-ratio-dict)))
+      (split-window-horizontally (floor (/ (window-body-width)
+                                           (1+ ratio-val)))))
+     (t
+      (split-window-horizontally)))
+    (set-window-buffer (next-window) (current-buffer))
+    (if (or (not ratio-val)
+            (>= ratio-val 1))
+        (windmove-right))))
+
+(defun my-split-window-vertically (&optional ratio)
+  "Split window vertically and resize the new window.
+'C-u number M-x my-split-window-vertically' uses pre-defined
+ratio from `my-ratio-dict'.
+Always focus on bigger window."
+  (interactive "P")
+  (let* (ratio-val)
+    (cond
+     (ratio
+      (setq ratio-val (cdr (assoc ratio my-ratio-dict)))
+      (split-window-vertically (floor (/ (window-body-height)
+                                         (1+ ratio-val)))))
+     (t
+      (split-window-vertically)))
+    ;; open another window with current-buffer
+    (set-window-buffer (next-window) (current-buffer))
+    ;; move focus if new window bigger than current one
+    (if (or (not ratio-val)
+            (>= ratio-val 1))
+        (windmove-down))))
+
+(global-set-key (kbd "C-x 2") 'my-split-window-vertically)
+(global-set-key (kbd "C-x 3") 'my-split-window-horizontally)
+
+(put 'erase-buffer 'disabled nil)
+(put 'downcase-region 'disabled nil)
 
 (provide '01-global)
