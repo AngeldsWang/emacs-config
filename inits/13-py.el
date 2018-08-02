06943c579ad204d130401ecf0059a34a41a32447
Zhenjun Wang <zjwang0414@gmail.com>
2018-08-02 09:40:24 +0800

    try jedi

--- inits/13-py.el
+++ inits/13-py.el
@@ -1,3 +1,10 @@
 (elpy-enable)
 
+(setq elpy-rpc-backend "jedi")
+
+(defun my/python-mode-hook ()
+  (add-to-list 'company-backends 'company-jedi))
+
+(add-hook 'python-mode-hook 'my/python-mode-hook)
+
 (provide '13-py)
