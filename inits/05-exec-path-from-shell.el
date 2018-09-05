e24f25c977f314542a3a87a026ad6ec34df64737
Zhenjun Wang <zjwang0414@gmail.com>
2018-09-05 17:28:14 +0800

    force read PATH from zshenv

--- inits/05-exec-path-from-shell.el
+++ inits/05-exec-path-from-shell.el
@@ -1,6 +1,7 @@
 (require 'exec-path-from-shell)
 (when (memq window-system '(mac ns))
   (setq exec-path-from-shell-arguments '("-l"))
+  (setq exec-path-from-shell-check-startup-files nil)
   (exec-path-from-shell-initialize)
   (exec-path-from-shell-copy-env "PATH")
   (exec-path-from-shell-copy-env "GOPATH"))
