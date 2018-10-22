2bd40b348bdd86ab3c53bc9f15007baaf06ce6fe
Zhenjun Wang <zjwang0414@gmail.com>
2018-10-22 11:10:03 +0800

    should keep before-save-hook locally

--- inits/12-cpp.el
+++ inits/12-cpp.el
@@ -52,7 +52,8 @@
 
 (global-set-key (kbd "C-c C-r") 'clang-format-region)
 (global-set-key (kbd "C-c C-f") 'clang-format-buffer)
-(add-hook 'c-mode-hook (lambda() (add-hook 'before-save-hook 'clang-format-buffer)))
-(add-hook 'c++-mode-hook (lambda() (add-hook 'before-save-hook 'clang-format-buffer)))
+
+(add-hook 'c-mode-hook (lambda() (add-hook 'before-save-hook 'clang-format-buffer nil t)))
+(add-hook 'c++-mode-hook (lambda() (add-hook 'before-save-hook 'clang-format-buffer nil t)))
 
 (provide '12-cpp)
