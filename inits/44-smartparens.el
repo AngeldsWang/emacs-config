f5aabe43ad6b8d4b2f8c9c66ec76ac3428fe3ac1
Zhenjun Wang <zjwang0414@gmail.com>
2018-10-23 13:09:06 +0800

    add auto newline and indent for smartparens

---
+++ inits/44-smartparens.el
@@ -0,0 +1,29 @@
+(require 'smartparens)
+(require 'smartparens-config)
+
+(sp-use-paredit-bindings)
+(add-hook 'malabar-mode-hook 'smartparens-mode)
+
+;; Function relating to customizing something relating to smartparens
+(defun my-create-newline-and-enter-sexp (&rest _ignored)
+  "Open a new brace or bracket expression, with relevant newlines and indent. "
+  (newline)
+  (indent-according-to-mode)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(defun customize-smartparens ()
+  "Customizes a part of smartparens.  Adds auto-expansion of {|} to {\n|\n} on press of enter with | being cursor."
+  (sp-local-pair 'c-mode "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
+  (sp-local-pair 'c++-mode "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
+  (sp-local-pair 'go-mode "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
+  (sp-local-pair 'cperl-mode "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
+  (sp-local-pair 'java-mode "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
+  (sp-local-pair 'sh-mode "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
+  (sp-local-pair 'rust-mode "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
+  (sp-local-pair 'js2-mode "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
+  (sp-local-pair 'ruby-mode "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET"))))
+
+(customize-smartparens)
+
+(provide '44-smartparens)
