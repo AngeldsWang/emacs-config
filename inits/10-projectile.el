9929564c42e4d9786d07c1049a62eb2ce37c227a
Zhenjun Wang <zjwang0414@gmail.com>
2018-11-09 20:45:56 +0800

    try speedup

--- inits/10-projectile.el
+++ inits/10-projectile.el
@@ -1,3 +1,5 @@
+(require 'projectile)
+
 (projectile-global-mode)
 (setq projectile-enable-caching t)
 (setq projectile-file-exists-remote-cache-expire nil)
@@ -9,5 +11,18 @@
 ;;(require 'projectile-speedbar)
 (add-hook 'projectile-mode-hook 'projectile-rails-on)
 (require 'go-projectile)
+
+(defadvice projectile-on (around exlude-tramp activate)
+    "This should disable projectile when visiting a remote file"
+    (unless  (--any? (and it (file-remote-p it))
+                     (list
+                      (buffer-file-name)
+                      list-buffers-directory
+                      default-directory
+                      dired-directory))
+      ad-do-it))
+
+;; don't try to find project name. Should speed up tramp sessions
+(setq projectile-mode-line "Projectile")
 
 (provide '10-projectile)
