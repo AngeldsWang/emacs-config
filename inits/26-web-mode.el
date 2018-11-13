89ce2af1dc205dcdbcbd09123259a8f163876f10
Zhenjun Wang <zjwang0414@gmail.com>
2018-11-13 11:49:38 +0800

    fix web-mode indent and add smartparens-off hook

--- inits/26-web-mode.el
+++ inits/26-web-mode.el
@@ -1,9 +1,9 @@
 (require 'web-mode)
 (require 'css-mode)
 
-(setq web-mode-markup-indent-offset 4)
-(setq web-mode-css-indent-offset 4)
-(setq web-mode-code-indent-offset 4)
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
 
 (setq web-mode-enable-auto-closing t)
 (setq web-mode-enable-auto-opening t)
@@ -16,6 +16,11 @@
 (add-to-list 'auto-mode-alist '("\\.erb$"  . web-mode))
 (add-to-list 'auto-mode-alist '("\\.twig$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mt$"   . web-mode))
+
+;; bind engines
+(setq web-mode-engines-alist
+      '(("django" . "\\.html\\'")
+        ("go"     . "\\.tmpl\\'")))
 
 ;; Use multiple-cursors to rename tags
 (define-key web-mode-map (kbd "C-c C-r") 'mc/mark-sgml-tag-pair)
@@ -44,5 +49,9 @@
 (define-key emmet-mode-keymap (kbd "C-j") nil)
 (setq emmet-move-cursor-between-quotes t)
 (setq emmet-move-cursor-after-expanding t)
+
+(defun my/web-mode-hook ()
+    (turn-off-smartparens-mode))
+(add-hook 'web-mode-hook 'my/web-mode-hook)
 
 (provide '26-web-mode)
