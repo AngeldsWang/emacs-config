296bcb35abaacebcc718b7da55f794a7f3f0c2e3
Zhenjun Wang <zjwang0414@gmail.com>
2018-12-03 13:17:01 +0800

    add youdao default config

---
+++ inits/73-dict.el
@@ -0,0 +1,9 @@
+(require 'youdao-dictionary)
+
+;; Enable Cache
+(setq url-automatic-caching t)
+
+;; Example Key binding
+(global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point)
+
+(provide '73-dict)
