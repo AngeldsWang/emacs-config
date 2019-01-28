4c8df7c50af3ff1204ff89d3caee496f07af69d1
Zhenjun Wang <zjwang0414@gmail.com>
2019-01-28 21:45:40 +0800

    miss multiple-cursors again

---
+++ inits/45-multi-line.el
@@ -0,0 +1,8 @@
+(require 'multiple-cursors)
+
+(global-set-key (kbd "C-c c") 'mc/edit-lines)
+(global-set-key (kbd "C-c n") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-c p") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c a") 'mc/mark-all-like-this)
+
+(provide '45-multi-line)
