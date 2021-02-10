f0beb8e0d3a3f031041454644def1bdac806873e
Zhenjun Wang <zjwang0414@gmail.com>
2021-02-10 14:40:56 +0800

    add custom symbol highlight colors

--- inits/52-highlight-symbol.el
+++ inits/52-highlight-symbol.el
@@ -5,4 +5,16 @@
 (global-set-key (kbd "S-<f3>") 'highlight-symbol-prev)
 (global-set-key (kbd "M-<f3>") 'highlight-symbol-remove-all)
 
+(setq highlight-symbol-colors
+      '("#DF8C8C" ;; red
+        "#F2C38F" ;; orange
+        "#DADA93" ;; yellow
+        "#A8CE93" ;; green
+        "#83AFE5" ;; blue
+        "#C97A7E" ;; old-rose
+        "#7FC1CA" ;; cyan
+        "#C1C4BE" ;; grey-nickel
+        "#9A93E1" ;; violet
+        ))
+
 (provide '52-highlight-symbol)
