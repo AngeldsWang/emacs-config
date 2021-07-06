09624d4b0ef6da0311ce4c36d5649032b126aea4
Zhenjun Wang <zjwang0414@gmail.com>
2021-07-06 20:58:42 +0800

    enable protobuf lint

--- inits/33-protobuf.el
+++ inits/33-protobuf.el
@@ -1,4 +1,20 @@
 (require 'protobuf-mode)
 (add-to-list 'auto-mode-alist '("\\.proto" . protobuf-mode))
 
+(require 'flycheck)
+(flycheck-define-checker protobuf-prototool
+  "prototool syntax checker"
+  :command ("prototool" "lint"
+            (eval (expand-file-name (buffer-file-name))))
+  :error-patterns
+  ((error line-start (file-name) ":" line ":" column
+          ":" (message) line-end))
+  :modes protobuf-mode
+  :predicate (lambda () (buffer-file-name)))
+
+(add-hook 'protobuf-mode-hook
+          (lambda ()
+            (flycheck-mode t)
+            (setq flycheck-checker 'protobuf-prototool)))
+
 (provide '33-protobuf)
