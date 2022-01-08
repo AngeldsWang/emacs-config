6aa6a532fd98748c2433c6496d61b96809ecd2f7
Zhenjun Wang <zjwang0414@gmail.com>
2022-01-08 11:43:09 +0800

    default jvm args

--- inits/18-java.el
+++ inits/18-java.el
@@ -21,4 +21,12 @@
             ;; use code format
             (add-hook 'before-save-hook 'lsp-format-buffer)))
 
+(setq lsp-java-vmargs '("-XX:+UseParallelGC" "-XX:GCTimeRatio=4" "-XX:AdaptiveSizePolicyWeight=90" "-Dsun.zip.disableMemoryMapping=true" "-Xmx8G" "-Xms100m"))
+;; https://emacs-lsp.github.io/lsp-mode/page/faq/#how-do-i-force-lsp-mode-to-forget-the-workspace-folders-for-multi-root
+(advice-add 'lsp :before (lambda (&rest _args) (eval '(setf (lsp-session-server-id->folders (lsp-session)) (ht)))))
+
+(defun use-gradle-version (gradle-ver)
+  (setq lsp-java-import-gradle-wrapper-enabled nil)
+  (setq lsp-java-import-gradle-version gradle-ver))
+
 (provide '18-java)
