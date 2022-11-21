b93bb8e8684b94efbc67e63d45da47c07b46e8e5
Zhenjun Wang <zjwang0414@gmail.com>
2022-11-22 00:21:51 +0800

    use pyright

--- inits/13-py.el
+++ inits/13-py.el
@@ -1,22 +1,38 @@
-(elpy-enable)
+(use-package python
+  :mode ("\\.py" . python-mode)
+  :ensure t)
 
-(setq elpy-rpc-backend "jedi")
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-pyright)
+                          (lsp))))
 
-(add-hook 'elpy-mode-hook (lambda ()
-                            (add-hook 'before-save-hook
-                                      'elpy-black-fix-code nil t)))
+(use-package python-black
+  :demand t
+  :after python
+  :hook (python-mode . python-black-on-save-mode))
 
-(when (load "flycheck" t t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
+;; (elpy-enable)
 
-(defun my/python-mode-hook ()
-  (add-to-list 'company-backends 'company-jedi))
+;; (setq elpy-rpc-backend "jedi")
+;; (setq elpy-rpc-python-command "python3")
 
-(add-hook 'python-mode-hook 'my/python-mode-hook)
+;; (add-hook 'elpy-mode-hook (lambda ()
+;;                             (add-hook 'before-save-hook
+;;                                       'elpy-black-fix-code nil t)))
 
-(setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args "-i --simple-prompt")
+;; (when (load "flycheck" t t)
+;;   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+;;   (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; (defun my/python-mode-hook ()
+;;   (add-to-list 'company-backends 'company-jedi))
+
+;; (add-hook 'python-mode-hook 'my/python-mode-hook)
+
+;; (setq python-shell-interpreter "ipython"
+;;       python-shell-interpreter-args "-i --simple-prompt")
 
 
 (provide '13-py)
