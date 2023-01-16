a24dd55187a5c230589b4c4de9ddcda2438e6810
Zhenjun Wang <zjwang0414@gmail.com>
2023-01-15 20:13:47 -0500

    update tramp

--- inits/53-tramp.el
+++ inits/53-tramp.el
@@ -1,18 +1,20 @@
+(require 'tramp)
 (setq tramp-default-method "ssh")
 
-(add-hook 'helm-tramp-pre-command-hook '(lambda ()
-                                          (projectile-mode 0)))
-(add-hook 'helm-tramp-quit-hook '(lambda ()
-                                   (projectile-mode 1)))
-
+(defadvice projectile-project-root (around ignore-remote first activate)
+  (unless (file-remote-p default-directory) ad-do-it))
+;; (add-hook 'helm-tramp-pre-command-hook '(lambda ()
+;;                                           (projectile-mode 0)))
+;; (add-hook 'helm-tramp-quit-hook '(lambda ()
+;;                                    (projectile-mode 1)))
 
 ;; ;; speed up tramp
-;; (setq remote-file-name-inhibit-cache nil)
-;; (setq vc-ignore-dir-regexp
-;;       (format "%s\\|%s"
-;;                     vc-ignore-dir-regexp
-;;                     tramp-file-name-regexp))
-;; (setq tramp-verbose 1)
+(setq remote-file-name-inhibit-cache nil)
+(setq vc-ignore-dir-regexp
+      (format "%s\\|%s"
+                    vc-ignore-dir-regexp
+                    tramp-file-name-regexp))
+(setq tramp-verbose 1)
 
 (setq make-backup-files nil)
 (setq create-lockfiles nil)
