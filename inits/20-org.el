de8ba12da8e5cb186503cb29f1152dac46a32c0a
Zhenjun Wang <zjwang0414@gmail.com>
2023-02-19 20:26:39 -0500

    try org-roam

--- inits/20-org.el
+++ inits/20-org.el
@@ -44,5 +44,27 @@
 (add-hook 'org-mode-hook
 	      (lambda () (local-set-key (kbd "C-c C-p") 'org-toggle-inline-images)))
 
+;; roam
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "~/work/my/note/orgs"))
+  :bind (("C-c m l" . org-roam-buffer-toggle)
+         ("C-c m f" . org-roam-node-find)
+         ("C-c m g" . org-roam-graph)
+         ("C-c m i" . org-roam-node-insert)
+         ("C-c m c" . org-roam-capture)
+         ;; Dailies
+         ("C-c m j" . org-roam-dailies-capture-today))
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (setq org-roam-completion-everywhere t)
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol)
+  (require 'org-roam-export))
+
+
 (provide '20-org)
 
