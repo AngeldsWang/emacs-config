f865258030960c17813dc1b9497e2b4c4827005a
Zhenjun Wang <zjwang0414@gmail.com>
2023-10-15 21:18:54 -0400

    no longer use youdao

--- inits/73-dict.el
+++ inits/73-dict.el
@@ -1,9 +1,7 @@
-(require 'youdao-dictionary)
-
-;; Enable Cache
-(setq url-automatic-caching t)
-
-;; Example Key binding
-(global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point)
+(use-package dictionary
+  :commands (dictionary-search)
+  :init
+  (global-set-key (kbd "C-c s") #'dictionary-search)
+  :config (setq dictionary-server "dict.org"))
 
 (provide '73-dict)
