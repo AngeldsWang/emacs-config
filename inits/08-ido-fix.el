8b086dc8b6be1c5ec7b7b727956e9f056c8db3b4
Zhenjun Wang <zjwang0414@gmail.com>
2023-10-15 21:18:25 -0400

    rebind isearch

--- inits/08-ido-fix.el
+++ inits/08-ido-fix.el
@@ -5,7 +5,7 @@
 ;; disable ido faces to see flx highlights.
 (setq ido-enable-flex-matching t)
 (setq ido-use-faces nil)
-(global-set-key (kbd "C-M-s") #'flx-isearch-forward)
+(global-set-key (kbd "C-M-i") #'flx-isearch-forward)
 (global-set-key (kbd "C-M-r") #'flx-isearch-backward)
 
 (provide '08-ido-fix)
